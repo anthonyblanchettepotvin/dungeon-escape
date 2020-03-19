3ec6ac99c37fba4a0ebca21973f85f3a2959db36
anthonyblanchettepotvin <anthony.blanchette.potvin@gmail.com>
2020-03-18 22:37:15 -0400

    Created IPuzzleTrigger, an interface that should be implemented by all puzzle trigger actors; Added the IPuzzleTrigger implementation in APuzzleButton, so any APuzzleButton can act as a trigger to any puzzle; Implemented the trigger system within IPuzzle and APuzzleImplementation.

--- Source/TempleEscape/PuzzleButton.h
+++ Source/TempleEscape/PuzzleButton.h
@@ -4,21 +4,35 @@
 
 #include "CoreMinimal.h"
 #include "BaseButton.h"
+#include "Puzzle.h"
 #include "PuzzleCondition.h"
+#include "PuzzleTrigger.h"
 #include "PuzzleButton.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class TEMPLEESCAPE_API APuzzleButton : public ABaseButton, public IPuzzleCondition
+class TEMPLEESCAPE_API APuzzleButton : public ABaseButton, public IPuzzleCondition, public IPuzzleTrigger
 {
 	GENERATED_BODY()
 
-protected:
+private:
+	virtual void Press() override;
+
 	virtual bool Evaluate_Implementation() override;
 	virtual bool Interact_Implementation() override;
 
+	virtual bool Trigger_Implementation() override;
+	virtual bool Subscribe_Implementation(AActor* Puzzle) override;
+	virtual bool Unsubscribe_Implementation(AActor* Puzzle) override;
+
+public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Puzzle")
 	TArray<APuzzleButton*> AdjacentButtons;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Puzzle")
+	TArray<AActor*> SubscribedPuzzles;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Puzzle", AdvancedDisplay)
+	bool bDebug = false;
 };
