42e54549ced80783efa3a2bf2f889560ba09ba27
anthonyblanchettepotvin <anthony.blanchette.potvin@gmail.com>
2020-03-22 00:40:28 -0400

    Started to make changes to the core mechanics of the puzzle system. The conditions and triggers notifies the puzzle instead of being checked by the puzzle each frame; Added the observer pattern necessary function signatures in IPuzzle (observer) and IPuzzleCondition (subject); Implemented those functions in APuzzleButton and APuzzleImplementation.

--- Source/TempleEscape/PuzzleButton.h
+++ Source/TempleEscape/PuzzleButton.h
@@ -21,17 +21,25 @@ private:
 	virtual void Press() override;
 
 	virtual bool Evaluate_Implementation() override;
+	virtual bool NotifyPuzzleStateChanged_Implementation() override;
+	virtual bool RegisterPuzzle_Implementation(AActor* Puzzle);
+	virtual bool UnregisterPuzzle_Implementation(AActor* Puzzle);
+
 	virtual bool Interact_Implementation() override;
 
 	virtual bool Trigger_Implementation() override;
 	virtual bool Subscribe_Implementation(AActor* Puzzle) override;
 	virtual bool Unsubscribe_Implementation(AActor* Puzzle) override;
 
+	virtual void SetIsPressed(bool Value) override;
+
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Puzzle")
 	TArray<APuzzleButton*> AdjacentButtons;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Puzzle")
 	TArray<AActor*> SubscribedPuzzles;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Puzzle")
+	TArray<AActor*> RegisteredPuzzles;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Puzzle", AdvancedDisplay)
 	bool bDebug = false;
